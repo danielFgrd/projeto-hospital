07cb08cd16c988d396d22a12b0d6c15579390d53
DanielFgrd <daniel.figueiredo01@etec.sp.gov.br>
2018-07-25 20:56:06 -0300

    Hello World concluido

---
+++ ProjetoHospital/src/main/java/br/com/danielfigueiredo/ProjetoHospital/persistence/JdbcUtil.java
@@ -0,0 +1,6 @@
+package br.com.danielfigueiredo.ProjetoHospital.persistence;
+
+
+public class JdbcUtil {
+
+}
---
+++ ProjetoHospital/src/main/java/br/com/danielfigueiredo/ProjetoHospital/view/App.java
@@ -0,0 +1,9 @@
+package br.com.danielfigueiredo.ProjetoHospital.view;
+
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
